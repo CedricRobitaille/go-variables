a8f091e69ed6ade35fea6ef5dbc74d197b1043a0
Cedric <crubbertire@rogers.com>
2026-04-14 14:13:00 -0700

    string encoding

--- variables.go
+++ variables.go
@@ -1,11 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func stringInterpolation() {
-	const name = "Cedric"
-	const openRate = 5.2
-
 	/*
 		For string interpolation:
 		- %v any value in default format ("i am %v years old", 23 OR "too many")
@@ -14,11 +14,29 @@ func stringInterpolation() {
 		- %f float values ("i am %.2f years old", 23.756)
 			- %.2f denotes decimal points.
 	*/
-	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent\n", name, openRate)
 
+	const name = "Cedric"
+	const openRate = 5.2
+
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent\n", name, openRate)
 	fmt.Print(msg)
 }
 
+func stringEncoding() {
+	/*
+		In Go, strings are a sequence of bytes that can hold any data.
+		Using the 'rune' type, it breaks the string into individual characters (where each can be more than 1 byte long)
+		Therefore, we can include various Unicode characters / chinese characters / whathaveyou.
+	*/
+
+	const name = "🚀"
+	fmt.Printf("constant 'name' byte length: %d\n", len(name))
+	fmt.Printf("constant 'name' rune length: %d\n", utf8.RuneCountInString(name))
+	fmt.Println("=========")
+	fmt.Printf("Hello %s\n", name)
+}
+
 func main() {
-	stringInterpolation()
+	// stringInterpolation()
+	stringEncoding()
 }
